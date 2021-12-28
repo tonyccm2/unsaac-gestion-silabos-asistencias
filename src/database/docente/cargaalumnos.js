dbd350dc26f6d31b4037ab865812089c24811156
tonyccm2 <tonyccm2@gmail.com>
2021-12-27 23:54:12 -0500

    funcion guardado de asistencia de un alumno

--- src/database/docente/cargaalumnos.js
+++ src/database/docente/cargaalumnos.js
@@ -13,6 +13,17 @@ var firebaseConfig = {
   
   const db = firebase.firestore();
 
+//guardar asistencia
+const saveAsistencia = (
+    codigo_ac,
+    fecha,
+    estado
+  ) =>
+    db.collection("asistencias").doc().set({
+        codigo_ac,
+        fecha,
+        estado
+    });
 //guardar alumno curso
 const saveAC = (
     nro_orden,
@@ -38,6 +49,8 @@ const saveAC = (
   
     const onGetAC = (callback) =>
       db.collection("acs").onSnapshot(callback);
+    //recupera 1 Courses por ID
+      const getACid = (id) => db.collection("acs").doc(id).get();
 
 const inputfileAlumnos = document.getElementById('inputfile-Alumnos')
 const alumnosContainer = document.getElementById("prelista-alumnos");   
@@ -115,6 +128,7 @@ formCargaAlumnos.addEventListener("submit", async (e) => {
   }
 });
 
+const formFecha = document.getElementById("formFecha");
 const acsContainer = document.getElementById("lista-acs");
 window.addEventListener("DOMContentLoaded", async (e) => {
   onGetAC((querySnapshot) => {
@@ -132,11 +146,61 @@ window.addEventListener("DOMContentLoaded", async (e) => {
               <td>${ac.ap}</td>
               <td>${ac.am}</td>
               <td>${ac.nombres}</td>
-              <td><input type="checkbox" class="sm-form-control"></td>
+              <td>
+                <button class="btn btn-primary btn-presente" data-id="${doc.id}">
+                Presente
+                </button>
+                <button class="btn btn-secondary btn-falto" data-id="${doc.id}">
+                Faltó
+                </button>
+              </td>
+
             </tr>
           </thead>
         </table>`;
       }
+    });
+  
+    //funcionalidad boton-asistencia
+    const btnsPresente = acsContainer.querySelectorAll(".btn-presente");
+
+    btnsPresente.forEach((btn) =>{
+      btn.addEventListener("click", async (e) => {
+        var fechaSelecionada = formFecha["fecha-lista"].value;
+        const doc = await getACid(e.target.dataset.id);
+        const ac = doc.data();
+        console.log(ac, fechaSelecionada, "asistió");
+        if(fechaSelecionada == ""){
+          alert("debe seleccionar una fecha para guardar la asistencia de los alumnos.")
+        }else{
+          saveAsistencia(
+            ac.codigo_ac,
+            fechaSelecionada,
+            "asistió"
+          );
+        }
+      })
+    });
+  
+    //funcionalidad boton-asistencia
+    const btnsFalto = acsContainer.querySelectorAll(".btn-falto");
+
+    btnsFalto.forEach((btn) =>{
+      btn.addEventListener("click", async (e) => {
+        var fechaSelecionada2 = formFecha["fecha-lista"].value;
+        const doc = await getACid(e.target.dataset.id);
+        const ac = doc.data();
+        console.log(ac, fechaSelecionada2, "faltó");
+        if(fechaSelecionada2 == ""){
+          alert("debe seleccionar una fecha para guardar la asistencia de los alumnos.")
+        }else{
+          saveAsistencia(
+            ac.codigo_ac,
+            fechaSelecionada2,
+            "faltó"
+          );
+        }
+      })
     });
   });
 });
